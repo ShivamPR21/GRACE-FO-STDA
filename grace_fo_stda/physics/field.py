98910c82fdfc781a7afa681394a3a68c32965e4d
ShivamPR21 <pandeyshivam2017robotics@gmail.com>
2020-12-03 16:40:13 +0530

    smd functions bug fix
    Signed-off-by: ShivamPR21 <pandeyshivam2017robotics@gmail.com>

--- grace_fo_stda/physics/field.py
+++ grace_fo_stda/physics/field.py
@@ -14,12 +14,14 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import numpy as np
 import os
+
+import numpy as np
 import pandas as pd
+import pyshtools as psh
 import scipy.io as sio
-import scipy.special as spc
 from matplotlib import pyplot as plt
+
 from .earthprams import EarthPrams
 
 
@@ -39,7 +41,7 @@ class GravityField(EarthPrams):
         Loads love number from love_numbers.xlsx
         :return:
         """
-        love_number = np.float32(pd.read_csv(self.dir_path+"/params/love_numbers.csv").values)
+        love_number = np.float32(pd.read_csv(self.dir_path + "/params/love_numbers.csv").values)
         return np.float32(love_number)
 
     def load_location_mask(self, loc, res=0.5):
@@ -48,8 +50,8 @@ class GravityField(EarthPrams):
         :param loc: String, Location name
         :param res: Float, Resolution of map 1 grid = res deg
         """
-        self.raster_map = sio.loadmat(self.dir_path+"/params/ctchmntindx3.mat")
-        self.catchment_info = sio.loadmat(self.dir_path+"/params/ctchnms.mat")["ctchnms"]
+        self.raster_map = sio.loadmat(self.dir_path + "/params/ctchmntindx3.mat")
+        self.catchment_info = sio.loadmat(self.dir_path + "/params/ctchnms.mat")["ctchnms"]
         catchment = np.concatenate(self.catchment_info[:, 0]).ravel()
         idx = np.concatenate(self.catchment_info[:, 3]).ravel()[np.where(catchment == loc)]
         # if idx is []:
@@ -84,50 +86,71 @@ class GravityField(EarthPrams):
                 l_range = np.arange(0, max_l + 1)
 
                 pre_multiplier = np.float64(
-                    [(2 * l_range + 1) / (1 + np.interp(l_range, self.love_number[:, 0], self.love_number[:, 1]))]).T
+                    [(2 * l_range + 1) / (
+                            1 + np.interp(l_range, self.love_number[:, 0], self.love_number[:, 1]))]).T.reshape(
+                    max_l + 1, 1, 1)
 
                 l, m = np.meshgrid(l_range, l_range)
                 l, m = l.reshape([np.size(l), 1]), m.reshape([np.size(m), 1])
 
-            smd_grid = np.zeros([np.max(self.smd_idx[:, 0])+1, np.max(self.smd_idx[:, 1])+1], dtype=np.float64)
+            smd_grid = np.zeros([np.max(self.smd_idx[:, 0]) + 1, np.max(self.smd_idx[:, 1]) + 1], dtype=np.float64)
             act_anomaly = np.float64(np.power(10, anomaly))
             act_anomaly[:, :, :2] = np.multiply(np.float64(anomaly_sign), act_anomaly[:, :, :2])
             # print(self.smd_idx)
             for (smd_idx, lat, long) in zip(self.smd_idx, self.lat, self.long):
                 # get YLM
-                ylm = spc.sph_harm(l, m, long, lat)
-                ylmc, ylms = np.float32(ylm.real), np.float32(ylm.imag)
-                ylmc, ylms = np.multiply(pre_multiplier, ylmc.reshape([max_l + 1, max_l + 1])), np.multiply(
-                    pre_multiplier, ylms.reshape([max_l + 1, max_l + 1]))
-                ylmc[np.isnan(ylmc)] = 0
-                ylms[np.isnan(ylms)] = 0
-                smd_grid[smd_idx[0], smd_idx[1]] = (self.a * self.rho / 3) * np.sum(
-                    [np.multiply(ylmc, act_anomaly[:, :, 0]), np.multiply(ylms, act_anomaly[:, :, 1])])
+                ylm = psh.expand.spharm(max_l, long, lat)
+                # ylm = spc.sph_harm(m, l, long, lat, ['4pi', 'real', 1, False, False])
+                # ylmc, ylms = np.float64(ylm[0, :, :]), np.float32(ylm.imag)
+                # ylmc, ylms = np.multiply(pre_multiplier, ylmc.reshape([max_l + 1, max_l + 1])), np.multiply(
+                #     pre_multiplier, ylms.reshape([max_l + 1, max_l + 1]))
+                # ylmc[np.isnan(ylmc)] = 0
+                # ylms[np.isnan(ylms)] = 0
+                ylm_ = np.zeros([max_l+1, max_l+1, 2], dtype=np.float64)
+                ylm_[:, :, 0] = ylm[0, :, :]
+                ylm_[:, :, 1] = ylm[1, :, :]
+                ylm_ = np.multiply(pre_multiplier, ylm_)
+
+                smd_grid[smd_idx[0], smd_idx[1]] = (self.R * self.rho / 3) * np.sum(
+                    [np.multiply(ylm_[:, :, 0], act_anomaly[:, :, 0]),
+                     np.multiply(ylm_[:, :, 0], act_anomaly[:, :, 1])])
             #
             # print(smd_grid[self.smd_idx[:, 0], self.smd_idx[:, 1]])
-            # plt.imshow(np.abs(smd_grid))
+            # print(smd_grid)
+            # plt.imshow(smd_grid)
             # plt.colorbar()
+            # plt.show()
             smd_info["smd_anomaly"].append(smd_grid)
-            break
+            # break
 
         for i in range(12):
             mean_anomaly = anomalies["sc_mean"]["month_" + str(i + 1)]
 
-            smd_grid = np.zeros([np.max(self.smd_idx[:, 0])+1, np.max(self.smd_idx[:, 1])+1], dtype=np.float64)
+            smd_grid = np.zeros([np.max(self.smd_idx[:, 0]) + 1, np.max(self.smd_idx[:, 1]) + 1], dtype=np.float64)
             act_anomaly = np.float64(np.power(10, mean_anomaly["mean_abs_log"]))
             act_anomaly[:, :, :2] = np.multiply(np.float64(mean_anomaly["mean_sign"]), act_anomaly[:, :, :2])
 
             for (smd_idx, lat, long) in zip(self.smd_idx, self.lat, self.long):
                 # get YLM
-                ylm = spc.sph_harm(l, m, long, lat)
-                ylmc, ylms = np.float32(ylm.real), np.float32(ylm.imag)
-                ylmc, ylms = np.multiply(pre_multiplier, ylmc.reshape([max_l + 1, max_l + 1])), np.multiply(
-                    pre_multiplier, ylms.reshape([max_l + 1, max_l + 1]))
-                ylmc[np.isnan(ylmc)] = 0
-                ylms[np.isnan(ylms)] = 0
-                smd_grid[smd_idx[0], smd_idx[1]] = (self.a * self.rho / 3) * np.sum(
-                    [np.multiply(ylmc, act_anomaly[:, :, 0]), np.multiply(ylms, act_anomaly[:, :, 1])])
+                ylm = psh.expand.spharm(max_l, long, lat)
+                # ylm = spc.sph_harm(m, l, long, lat)
+                # ylmc, ylms = np.float32(ylm.real), np.float32(ylm.imag)
+                # ylmc, ylms = np.multiply(pre_multiplier, ylmc.reshape([max_l + 1, max_l + 1])), np.multiply(
+                #     pre_multiplier, ylms.reshape([max_l + 1, max_l + 1]))
+                # ylmc[np.isnan(ylmc)] = 0
+                # ylms[np.isnan(ylms)] = 0
+                ylm_ = np.zeros([max_l + 1, max_l + 1, 2], dtype=np.float64)
+                ylm_[:, :, 0] = ylm[0, :, :]
+                ylm_[:, :, 1] = ylm[1, :, :]
+                ylm_ = np.multiply(pre_multiplier, ylm_)
 
+                smd_grid[smd_idx[0], smd_idx[1]] = (self.R * self.rho / 3) * np.sum(
+                    [np.multiply(ylm_[:, :, 0], act_anomaly[:, :, 0]),
+                     np.multiply(ylm_[:, :, 1], act_anomaly[:, :, 1])])
+
+            # plt.imshow(np.abs(smd_grid))
+            # plt.colorbar()
+            # return
             smd_info["smd_mean"].update({"month_" + str(i + 1): smd_grid})
 
         return smd_info
